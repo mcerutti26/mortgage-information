e40cfcc80c2c800d58501416592b228e2117faf2
mcerutti <mcerutti26@gmail.com>
2023-02-05 21:26:35 -0500

    Amoritization calculation function and initial PySimpleGUI visualization

--- _base.py
+++ _base.py
@@ -16,7 +16,18 @@ class Mortgage:
     def monthly_payment(self):
         total_payment = (self.saleprice-self.downpayment)*(self.monthly_rate*((1+self.monthly_rate)**self.n_compound))/(((1+self.monthly_rate)**self.n_compound)-1)
         return total_payment
-    def monthly_interest(self):
-        pass
-    def monthly_principal(self):
-        pass
+    def calc_amoritization(self):
+        # interest = P * (interest rate/compounds per year)
+        df = pd.DataFrame()
+        df['Month'] = list(range(1,(self.length*12)+1))
+        df['Interest Due'] = 0
+        df['Principal Due'] = 0
+        principal = self.saleprice - self.downpayment
+        total_monthly_payment = self.monthly_payment()
+        for index, data in df.iterrows():
+            cur_interest_payment = principal * (self.rate/12)
+            data['Interest Due'] = cur_interest_payment
+            cur_principal_payment = total_monthly_payment-cur_interest_payment
+            data['Principal Due'] = cur_principal_payment
+            principal = principal - cur_principal_payment
+        return df
--- decision_maker.py
+++ decision_maker.py
@@ -1,6 +1,14 @@
 from _base import Mortgage
+import PySimpleGUI as sg
 
 if __name__ == '__main__':
-    cur_mort = Mortgage(795000,(79500+5000),5.75,30,True)
+    cur_mort = Mortgage(500000,(60000),6.25,30,True)
     pay = cur_mort.monthly_payment()
     print('Your monthly P&I is $' + str(round(pay, 2)) + '.')
+    amoritization_df = cur_mort.calc_amoritization()
+    headings = list(amoritization_df.columns)
+    values = amoritization_df.values.tolist()
+    layout = [[sg.Table(values=values,font=72, headings=headings,key='-TABLE-',expand_x=True,expand_y=True,auto_size_columns=True,justification='center',alternating_row_color='blue')]]
+    window = sg.Window('Amoritization Chart', layout, size=(400,400),resizable=True)
+    event, value = window.read()
+    print(amoritization_df)
